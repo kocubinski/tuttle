b60d6a0e925cb52fce36a6923f11e7da1444f6af
Matt Kocubinski <mkocubinski@gmail.com>
2018-11-21 13:13:42 -0500

    No need for sed replace in yaml

--- build.sh
+++ build.sh
@@ -2,13 +2,8 @@
 set -e
 
 [[ -n $REPO ]] || REPO=kocubinski
-: ${NAMESPACE?"Need to set NAMESPACE"}
 
 IMAGE=$REPO/tuttle:latest
 
 docker build -t $IMAGE .
 docker push $IMAGE
-
-cat deployment.yaml \
-  | sed -e "s|@IMAGE@|$IMAGE|g;" \
-  | kubectl apply -n $NAMESPACE -f -
